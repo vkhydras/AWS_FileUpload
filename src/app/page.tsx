5298c7ade867125c161427f8dbc4e415f75dbd7e
vkhydras <victorkimaru8@gmail.com>
2024-02-05 15:43:29 +0300

    link to upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,8 @@ import { useRouter } from "next/navigation";
 export default function Home() {
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <button ><Link href='/upload'>Upload Form</Link></button>
     
-    </main>
+   
   );
 }
