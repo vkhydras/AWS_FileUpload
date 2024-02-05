1630ea643bd37ac86a5c899bd380c7aaebaddc51
vkhydras <victorkimaru8@gmail.com>
2024-02-05 15:42:34 +0300

    link to uplaod form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 "use client";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
-  const goToUpload = () => {
-    router.replace("/upload");
-  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <button onClick={goToUpload}>Go To Upload Form</button>
+      <button ><Link href='/upload'>Upload Form</Link></button>
+    
     </main>
   );
 }
